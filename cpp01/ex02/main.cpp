e29ecc1dcd6c2379e37b0e9840275d2c0461288a
Katsiaryna <katyamulya@gmail.com>
2026-03-05 11:26:59 +0100

    small correction for ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string* stringPTR = &str;
-    std::string& stringREF = str;
+    std::string* stringPTR = &str;  //pointer
+    std::string& stringREF = str;   //reference
 
     std::cout << &str << std::endl;       //The memory address of the string variable
     std::cout << stringPTR << std::endl;  //The memory address held by stringPTR
@@ -22,4 +22,8 @@ int main()
 a reference works as an alias for an existing variable, 
 providing an alternative name for it and allowing you 
 to work with the original data directly.
+A reference must be initialized when declared and cannot be null.
+Use references when you can, and pointers only when you have to 
+(such as for optional values that might be null or for implementing 
+complex data structures like linked lists)
 */
