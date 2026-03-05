bdb2b073d38cda74f8411d7f2597e0db9c1ae528
Katsiaryna <katyamulya@gmail.com>
2026-03-05 11:51:19 +0100

    init list changed oder

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name(name), weapon(nullptr) {}
+HumanB::HumanB(std::string name) : weapon(nullptr), name(name) {}
 
 HumanB::~HumanB() {}
 
